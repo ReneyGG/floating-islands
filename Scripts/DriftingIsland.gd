050920ca26ce279f525ca28e06c5379be1090a74
ReneyGG <jakubczyzycki12@gmail.com>
2023-05-13 12:08:12 +0200

    fixed velocity cancel and added both one way collisions

--- Scripts/DriftingIsland.gd
+++ Scripts/DriftingIsland.gd
@@ -4,7 +4,13 @@ var velocity = Vector2.ZERO
 export var drift_speed = 0
 
 func _ready():
-	velocity.x += drift_speed
+	#velocity.x += drift_speed
+	
+	pass
 
 func _physics_process(delta):
-	velocity = move_and_slide(velocity, Vector2.UP)
+	#velocity = move_and_slide(velocity, Vector2.UP)
+	if drift_speed >= 0:
+		global_position = global_position.move_toward(Vector2(3000,global_position.y), delta * drift_speed)
+	else:
+		global_position = global_position.move_toward(Vector2(-3000,global_position.y), delta * (drift_speed * -1))
