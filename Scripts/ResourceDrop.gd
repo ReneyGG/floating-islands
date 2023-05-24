fe1ca98501254d6dcfddad1293b4468e1c3d2ac2
ReneyGG <jakubczyzycki12@gmail.com>
2023-05-24 11:30:16 +0200

    added all 5 resource drop scenes

--- Scripts/ResourceDrop.gd
+++ Scripts/ResourceDrop.gd
@@ -1,7 +1,6 @@
 extends RigidBody2D
 
-var type = "floatstone"
-var sprite = "res://Assets/Placeholders/mineral-deposit-hanging.png"
+export var type = "floatstone"
 var go = false
 var pos
 var end = false
@@ -9,7 +8,6 @@ var end = false
 onready var player = get_parent().get_node("Player")
 
 func _ready():
-	#get_node("Sprite").texture = load(sprite)
 	var random = RandomNumberGenerator.new()
 	random.randomize()
 	var liny = random.randi_range(320,400)
