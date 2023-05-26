962fcf07f92b52a357f09e9ae2dfeaaab3b7e2b2
ReneyGG <jakubczyzycki12@gmail.com>
2023-05-26 16:32:12 +0200

    added ore generation for each deposit on spawned islands

--- Scripts/DriftingIsland.gd
+++ Scripts/DriftingIsland.gd
@@ -5,12 +5,9 @@ export var drift_speed = 0
 export var island_value = 0
 
 func _ready():
-	#velocity.x += drift_speed
-	
 	pass
 
 func _physics_process(delta):
-	#velocity = move_and_slide(velocity, Vector2.UP)
 	if drift_speed >= 0:
 		global_position = global_position.move_toward(Vector2(3000,global_position.y), delta * drift_speed)
 	else:
--- Scripts/IslandSpawner.gd
+++ Scripts/IslandSpawner.gd
@@ -10,6 +10,12 @@ export var drift_direction = -1
 export var position_jitter = 500
 export var higher_relative_position: bool = false
 
+export var floatstone = 1
+export var aetheric_iron = 1
+export var celestium = 1
+export var azulite = 1
+export var sky_ruby = 1
+
 var rng = RandomNumberGenerator.new()
 onready var spawn_timer = get_node("IslandSpawnTimer")
 
@@ -18,17 +24,30 @@ func _ready():
 		self.z_index = 0
 	else:
 		self.z_index = 3
-	rng.randomize()
 	spawn_timer.set_wait_time(1)
 	spawn_timer.start()
 
+func generate_ore():
+	var sum = floatstone+aetheric_iron+celestium+azulite+sky_ruby
+	var dict = {"Floatstone":floatstone,"AethericIron":aetheric_iron+floatstone,"Celestium":celestium+aetheric_iron+floatstone,"Azulite":azulite+celestium+aetheric_iron+floatstone,"SkyRuby":sum}
+	var r = rng.randi_range(1,sum)
+	for i in dict:
+		if dict[i] >= r:
+			return i
+
 func _on_IslandSpawnTimer_timeout():
+	rng.randomize()
 	# Generate a new island with some positional jitter and give it a random drift speed.
 	var new_island = possible_islands[rng.randi() % len(possible_islands)].instance()
 	new_island.drift_speed = rng.randf_range(60.0, 100.0)*drift_direction
 	new_island.position.y += rng.randi_range(-position_jitter, position_jitter)
 	new_island.z_index = self.z_index
 	add_child(new_island)
+	# generate ore deposits
+	for i in new_island.get_children():
+		if i.is_in_group("ore"):
+			var type = generate_ore()
+			i.resource_drop = load("res://Scenes/ResourceDeposits/"+type+"/"+type+"Drop.tscn")
 	
 	_randomize_timer()
 
